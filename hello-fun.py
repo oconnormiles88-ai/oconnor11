aaca64f2aebbb545c962dc897832a423274a05cb
oconnormiles88-ai <oconnormiles88@gmail.com>
2026-09-23 17:18:22 -0400

    1

---
+++ hello-fun.py
@@ -0,0 +1,8 @@
+def say_hello(to):
+    print("Hello" , to)
+
+    your_name = input("What is your name? ").title()
+    Spouse_name = input("What is your spouse's name? ").title()
+
+    say_hello(your_name)
+    say_hello(Spouse_name)
